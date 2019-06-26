54e9ee0cd661aec213b221acfc4672fa3b66ef28
ying1332 <49078511+ying1332@users.noreply.github.com>
2019-06-26 14:04:29 -0400

    added comments to run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from main_menu import MainMenu
+from main_menu import MainMenu #Import Main Game Loop
 
 if __name__ == '__main__':
+    #run the game
     mainmenu = MainMenu()
     mainmenu.__run__()
